f226a70dbe1d8b23851b6922ff2f15b7113a16d5
tibor-cernak <tibor.cernak4@gmail.com>
2025-01-13 11:10:59 +0100

    allow command-line args to decide which mode to use

--- main.cpp
+++ main.cpp
@@ -1,18 +1,26 @@
 #include "src/ConfigurationMode.h"
 #include "src/OperatingMode.h"
+#include <iostream>
+#include <string>
 
-int main() {
-bool config = false;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " --config | --operate\n";
+        return 1;
+    }
 
-if (config){
-    ConfigurationMode gui;
-    gui.run();
+    std::string mode = argv[1];
+
+    if (mode == "--config") {
+        ConfigurationMode gui;
+        gui.run();
+    } else if (mode == "--operate") {
+        OperatingMode operatingMode;
+        operatingMode.run();
+    } else {
+        std::cerr << "Invalid flag. Use --config or --operate.\n";
+        return 1;
+    }
+
+    return 0;
 }
-else{
-    OperatingMode operatingMode;
-    operatingMode.run();
-}
-
-
-}
-
