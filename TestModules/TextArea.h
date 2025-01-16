6363a23d790c1bc6b742e9d954c6ed15aabcf230
Krajcovic70 <mato.bsc@gmail.com>
2025-01-16 17:54:26 +0100

    added class for text area that will be using scrollbar instead of modules

---
+++ TestModules/TextArea.h
@@ -0,0 +1,14 @@
+//
+// Created by X280 on 16. 1. 2025.
+//
+
+#ifndef SMELYZAJKO_GUI_TEXTAREA_H
+#define SMELYZAJKO_GUI_TEXTAREA_H
+
+
+class TextArea {
+
+};
+
+
+#endif //SMELYZAJKO_GUI_TEXTAREA_H
