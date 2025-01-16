11f38c6f4c180f805e64760fa6be90baf54380ce
Krajcovic70 <mato.bsc@gmail.com>
2025-01-16 15:57:06 +0100

    empty class for scrollbar

---
+++ TestModules/Scrollbar.cpp
@@ -0,0 +1,5 @@
+//
+// Created by X280 on 16. 1. 2025.
+//
+
+#include "Scrollbar.h"
---
+++ TestModules/Scrollbar.h
@@ -0,0 +1,14 @@
+//
+// Created by X280 on 16. 1. 2025.
+//
+
+#ifndef SMELYZAJKO_GUI_SCROLLBAR_H
+#define SMELYZAJKO_GUI_SCROLLBAR_H
+
+
+class Scrollbar {
+
+};
+
+
+#endif //SMELYZAJKO_GUI_SCROLLBAR_H
