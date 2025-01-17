3262a1c846d5aac2bb12d4c5dc3ddbb90a91bbb6
tibor-cernak <tibor.cernak4@gmail.com>
2025-01-17 11:43:48 +0100

    changed Slider class to act as parent class for Horizontal and Vertical Sliders

--- src/widgets/Slider.h
+++ src/widgets/Slider.h
@@ -1,9 +1,11 @@
+#pragma once
+
 #include "Element.h"
 #include <algorithm>
 
 template<typename E> class Slider : public Element {
     static_assert(std::is_arithmetic<E>::value, "Slider can only work with numeric types (int, float, etc.)");
-private:
+protected:
     ImVec2 size;
     E value;
     E minValue;
@@ -17,13 +19,14 @@ public:
     E getValue() const;
     void setValue(E val);
 
-    void draw(ImGuiIO& io) override;
-    void handleClicks(ImGuiIO &io) override;
+    virtual void draw(ImGuiIO& io) override = 0;
+    virtual void handleClicks(ImGuiIO &io) override = 0;
     ImRect getBoundingBox() const override;
 
     float getNormalizedValue() const;
 
-    void from_json(const nlohmann::json &j, ImVec2 resolution) override;
-    void to_json(nlohmann::json &j) const override;
+    virtual void from_json(const nlohmann::json &j, ImVec2 resolution) override;
+    virtual void to_json(nlohmann::json &j) const override = 0;
+
     std::vector<Setting> getSettings() override;
 };
